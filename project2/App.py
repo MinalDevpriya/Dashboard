691bdc9c92b44056c83dceb77af6760748b60ecd
MinalDevpriya <73357219+MinalDevpriya@users.noreply.github.com>
2023-02-27 14:56:55 +0530

    Add files via upload

---
+++ project2/App.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title("Minal Devpriya")
+st.header("Data Science Intern at Innomatics Research Lab")
+
+
+
+btn_click=st.button("Education Details")
+if btn_click == True:
+    st.header("Manipal University Jaipur")
+    st.subheader("B.Tech-Electronics and Communication")
+    st.subheader("2019-2023")
+    st.subheader("CGPA-7.99")
